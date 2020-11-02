b53e71d2cdd7b974726f8874f0bc79f69df3dfef
Joel Gustafson <joelg@mit.edu>
2020-11-01 19:50:00 -0500

    update: light login page styling

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,19 +1,23 @@
 import React, { useState } from "react";
 import { signIn } from "next-auth/client";
-import { Button } from "evergreen-ui";
+import { Pane, Button, TextInput, majorScale } from "evergreen-ui";
 
 const Login = () => {
 	const [email, setEmail] = useState("");
 	return (
-		<div>
-			<input
+		<Pane marginY={majorScale(12)} display="flex" justifyContent="center" flexWrap="wrap">
+			<TextInput
+				margin={majorScale(1)}
 				value={email}
-				onChange={(evt) => {
-					setEmail(evt.target.value);
-				}}
+				onChange={({ target: { value } }: React.ChangeEvent<HTMLInputElement>) =>
+					setEmail(value)
+				}
+				placeholder="alice@example.com"
 			/>
-			<Button onClick={() => signIn("email", { email })}>Sign in with Email</Button>
-		</div>
+			<Button margin={majorScale(1)} onClick={() => signIn("email", { email })}>
+				Sign in with Email
+			</Button>
+		</Pane>
 	);
 };
 
