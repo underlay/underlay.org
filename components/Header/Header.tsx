d5e8884e9cad7bdd451af4dbabbc7f7976423f2d
Joel Gustafson <joelg@mit.edu>
2020-11-05 20:09:41 -0500

    dev: added semver dependency and type declarations

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Button, IconButton } from "evergreen-ui";
+import { Button, IconButton, Menu, Pane, Popover, Position } from "evergreen-ui";
 
 import { Avatar } from "components";
 import { usePageContext } from "utils/client/hooks";
 
 import styles from "./Header.module.scss";
+import { signOut } from "next-auth/client";
 
 const Header = () => {
 	const { session } = usePageContext();
@@ -12,36 +13,65 @@ const Header = () => {
 	return (
 		<nav className={styles.header}>
 			<div className={styles.content}>
-				<div className={styles.left}>
-					<Button
-						height={40}
-						appearance="minimal"
-						is="a"
-						href="/"
-						className={styles.logo}
-					>
+				<Pane>
+					<Button height={40} appearance="minimal" is="a" href="/">
 						R1
 					</Button>
-				</div>
-				<div>
+				</Pane>
+				<Pane display="flex" alignItems="center">
 					{user ? (
-						<IconButton
-							appearance="minimal"
-							height={40}
-							icon={
-								<Avatar
-									size={32}
-									name={user.email}
-									src={user.avatar || undefined}
+						<>
+							<Button is="a" href="/new/schema" marginRight={12} appearance="minimal">
+								New schema
+							</Button>
+							<Popover
+								position={Position.BOTTOM_LEFT}
+								content={
+									<Menu>
+										<Menu.Group>
+											<Menu.Item
+												is="a"
+												href={`/${user.slug}`}
+												disabled={true}
+											>
+												Profile
+											</Menu.Item>
+											<Menu.Item
+												is="a"
+												href={`/${user.slug}/settings`}
+												disabled={true}
+											>
+												Settings
+											</Menu.Item>
+										</Menu.Group>
+										<Menu.Divider />
+										<Menu.Group>
+											<Menu.Item onSelect={() => signOut()}>
+												Log out
+											</Menu.Item>
+										</Menu.Group>
+									</Menu>
+								}
+							>
+								<IconButton
+									appearance="minimal"
+									height={40}
+									icon={
+										<Avatar
+											size={32}
+											name={user.email}
+											src={user.avatar || undefined}
+										/>
+									}
 								/>
-							}
-						/>
+							</Popover>
+						</>
 					) : (
 						<Button is="a" href="/login" appearance="minimal" height={40}>
 							Login
 						</Button>
 					)}
-				</div>
+				</Pane>
 			</div>
 		</nav>
 	);
