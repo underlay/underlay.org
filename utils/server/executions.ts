f6d9b1a59a3a3aa931139e3dfdab0e18a5a29ed4
Joel Gustafson <joelg@mit.edu>
2021-04-13 18:19:05 -0400

    add custom execution number format

--- utils/server/executions.ts
+++ utils/server/executions.ts
@@ -1,7 +1,7 @@
 import { StatusCodes } from "http-status-codes";
 import { ApiError } from "next-rest/server";
 
-export const initialExecutionNumber = "0";
+export const initialExecutionNumber = "x0";
 export function getExecutionNumber(execution: { executionNumber: string } | null) {
 	if (execution === null) {
 		return initialExecutionNumber;
@@ -10,12 +10,13 @@ export function getExecutionNumber(execution: { executionNumber: string } | null
 	}
 }
 
-const executionPattern = /^\d+$/;
+const executionPattern = /^x(\d+)$/;
 
 function incrementExecutionNumber(executionNumber: string) {
-	if (executionPattern.test(executionNumber)) {
-		const number = parseInt(executionNumber);
-		return (number + 1).toString();
+	const match = executionPattern.exec(executionNumber);
+	if (match !== null) {
+		const [_, n] = match;
+		return `x${parseInt(n) + 1}`;
 	} else {
 		throw new ApiError(StatusCodes.INTERNAL_SERVER_ERROR);
 	}
