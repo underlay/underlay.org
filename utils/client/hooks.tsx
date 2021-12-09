790c4609b2454a9b198b7cdbaeaf7eb9c8d82cea
Travis Rich <travis.s.rich@gmail.com>
2021-12-09 15:21:09 -0500

    fix: location init

--- utils/client/hooks.tsx
+++ utils/client/hooks.tsx
@@ -4,5 +4,5 @@ import { LocalUserData, LocationData } from "utils/shared/types";
 export const LoginContext = React.createContext<LocalUserData>(undefined);
 export const useLoginContext = (): LocalUserData => useContext(LoginContext);
 
-export const LocationContext = React.createContext<LocationData>({});
+export const LocationContext = React.createContext<LocationData>({ pathname: "", query: {} });
 export const useLocationContext = (): LocationData => useContext(LocationContext);
