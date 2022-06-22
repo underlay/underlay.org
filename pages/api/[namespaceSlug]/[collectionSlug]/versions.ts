8321f8b6c60c1d6be50fd9bf2466578824e4047e
Pine Wu <octref@gmail.com>
2022-06-22 10:52:37 -0400

    feat: api for fetching all versions of a collection. fix #80

---
+++ pages/api/[namespaceSlug]/[collectionSlug]/versions.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import nextConnect from "next-connect";
+import prisma from "prisma/db";
+
+export default nextConnect<NextApiRequest, NextApiResponse>().get(async (req, res) => {
+	const namespaceSlug = req.query.namespaceSlug as string;
+	const collectionSlug = req.query.collectionSlug as string;
+
+	const colSlugSuffix = collectionSlug.split("-").pop();
+
+	if (namespaceSlug && collectionSlug) {
+		const collection = await prisma.collection.findFirst({
+			where: {
+				slugSuffix: { equals: colSlugSuffix },
+			},
+			include: {
+				versions: true,
+			},
+		});
+
+		if (!collection) {
+			return res.status(400).json({ ok: false });
+		}
+
+		return res.status(200).json({ data: collection.versions });
+	}
+});
